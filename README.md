cfe9c56e6277c2772f244d98b56ac44e15530b3f
tra38 <tra38@nau.edu>
2018-09-23 01:50:26 -0500

    Declare victory and go home

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # nanogenmo-2018
-A computer program intended to generate the novella "The Computer Crashes"
+A computer program intended to generate the novella "The Computer Crashes".
+
+Novella Length, Without Citations: 11,190 words
+
+Novella Length, With Citations: 11,493 words
+
+At the end of the novella, I added some 'meows' to increase my word count to >50K, thereby qualifying this as a NaNoGenMo novel (though one that was not done in November).
+
+Novella Length, Including Junk Meows: 61,504 words
---
+++ story_compiler.py
@@ -0,0 +1,89 @@
+import json
+import os
+import random
+
+# set up boilerplate methods
+
+# Append text to the novel file
+def write_to_novel(text):
+  with open('novel.md', 'a') as out:
+      out.write(text + '\n\n')
+
+# Randomize an array
+def randomize_array(array):
+  random.seed(42) # fixed seed ensures reproducability
+  random.shuffle(array)
+  return array
+
+# Generate a citation in the form of '{shortened version of quote in story} is from {source}, {license_link}. Quote modified from the original - original quote is {shortened version of original quote}'
+def generate_citation(passage):
+  shortened_new_quote = generate_quote_shortner(passage['quote'])
+  shortened_original_quote = generate_quote_shortner(passage['original_quote'])
+  source_link = passage['source']
+  license_link = generate_link(passage['license'], passage['license_url'])
+  template = "- {shortened_new_quote} is from {source_link}, {license_link}. Quote modified from original - original quote is {shortened_original_quote}"
+  final_response = template.format(shortened_new_quote=shortened_new_quote, shortened_original_quote=shortened_original_quote, source_link=source_link, license_link=license_link)
+  return final_response
+
+# generate a shortened version of the quote in the form of '{first three words} ... {last three words}'
+def generate_quote_shortner(quote):
+  text_array = quote.split()
+  first_phrase = ' '.join(text_array[0:3])
+  second_phrase = ' '.join(text_array[-3:])
+  template = '"{first_phrase} ... {second_phrase}"'
+  final_response = template.format(first_phrase=first_phrase, second_phrase=second_phrase)
+  return final_response
+
+# Generate a Markdown-formatted version of the link
+def generate_link(license, license_url):
+  license_link_template = "[{license}]({license_url})"
+  license_link = license_link_template.format(license=license, license_url=license_url)
+  return license_link
+
+# remove our existing novel if it already exists
+try:
+    os.remove("novel.md")
+except OSError:
+    pass
+
+# Now let's load up our data, making sure to randomize the order of the paragraphs in the middle section
+with open("corpus.json", 'r') as stream:
+  data = json.load(stream)
+
+starting_paragraphs = data['begin']
+middle_paragraphs = randomize_array(data['missions'])
+ending_paragraphs = data['ending']
+
+# Write the introductory paragraphs, with a horizontal bar after the introduction
+for passage in starting_paragraphs:
+  write_to_novel(passage['quote'])
+write_to_novel("---")
+
+# Write the missions, with a horizontal bar after each mission
+for passage in middle_paragraphs:
+  write_to_novel(passage['quote'])
+  write_to_novel("---")
+
+# Write the ending paragraph, with a horizontal bar after the ending
+for passage in ending_paragraphs:
+  write_to_novel(passage['quote'])
+write_to_novel("---")
+
+# Now, let's write the citations!
+
+write_to_novel("Citations:")
+
+all_paragraphs = starting_paragraphs + middle_paragraphs + ending_paragraphs
+
+for passage in all_paragraphs:
+  write_to_novel(generate_citation(passage))
+
+# And just a little wrapup to meet word counts
+
+write_to_novel("---")
+
+write_to_novel("The following is a summary from the novel 'The Moloch Diaries'...")
+
+the_final_welp = "meow " * 50000
+
+write_to_novel(the_final_welp)
